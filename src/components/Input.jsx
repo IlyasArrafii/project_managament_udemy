c1a11f829044ccbe4fbd15464a0edbeef7ffca29
IlyasArrafii <zainularrafii@gmail.com>
2024-12-27 21:36:13 +0700

    input styling and button styling

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,8 +1,14 @@
 export default function Input({ label, textarea, ...props }) {
+    const classes = 'w-full rounded-md border-black bg-stone-200 p-1 text-black';
+
     return (
-        <p>
-            <label>{label}</label>
-            {textarea ? <textarea {...props} /> : <input {...props} />}
+        <p className="my-4 flex flex-col gap-1">
+            <label className="text-lg font-bold uppercase text-black">{label}</label>
+            {textarea ? (
+                <textarea className={classes} {...props} />
+            ) : (
+                <input className={classes} {...props} />
+            )}
         </p>
     );
 }
--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -2,13 +2,13 @@ import Input from './Input.jsx';
 
 export default function NewProject() {
     return (
-        <div className='py-8'>
-            <menu>
+        <div className="w-[35rem] py-8">
+            <menu className="my-4 flex items-center justify-end gap-4">
                 <li>
-                    <button>Cancel</button>
+                    <button className="hover:bg-gray-6800 rounded-md bg-gray-500 px-6 py-2 text-white">Cancel</button>
                 </li>
                 <li>
-                    <button>Save</button>
+                    <button className="rounded-md bg-blue-500 px-6 py-2 text-white hover:bg-blue-600">Save</button>
                 </li>
             </menu>
             <div>
